70672b999dac429abbbdc71bc929e592553d0a0a
Mohamed Azhar <muhammedazhar2k18@gmail.com>
2023-05-30 19:28:47 +0530

    first commit

---
+++ list_comprehensive.py
@@ -0,0 +1,17 @@
+# result=[]
+# # # for i in "inmakes":
+# # #     result.append(i)
+# # # print(result)
+# # result=[i for i in "inmakes"]
+# # print(result)
+
+
+names=["azhar","aslah","asim"]
+# new=[]
+# for i in names:
+#     if 's' in i:
+#         new.append(i)
+# print(i)
+
+new=[i for i in names if 'a' in i]
+print(new) 
